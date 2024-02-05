83a97fd02fd040677c23c347867723f55bc4e04c
brandon (OB) <brandon.lim@oneberry.com>
2024-02-05 18:28:23 +0800

    Let's try making as a react app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+      <Navbar/>
+    </div>
+  );
+}
